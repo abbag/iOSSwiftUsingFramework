a5072b13c3996855706d55fda51ba7822942156e
Zheng-Han_Wu <Zheng-Han_Wu@asus.com>
2019-05-31 15:19:01 +0800

    1. [Git] Modify .gitignore. 2. [Test] Create app/framework projects. 3. [App/Framework] Add test ViewControllers and flow. --

---
+++ TestFramework02/TestFramework02/TestFramework02.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework02.h
+//  TestFramework02
+//
+//  Created by Mac on 5/31/19.
+//  Copyright © 2019 Homer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework02.
+FOUNDATION_EXPORT double TestFramework02VersionNumber;
+
+//! Project version string for TestFramework02.
+FOUNDATION_EXPORT const unsigned char TestFramework02VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework02/PublicHeader.h>
+
+
